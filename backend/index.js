39a45bccbf8571fe999f0b4c7f6f1f387f0cab03
123quatrecinq <96394817+AmielCo@users.noreply.github.com>
2022-05-23 17:39:02 +0300

    hotel routing

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,14 @@
 import express from "express";
 import "dotenv/config";
 import mongoose from "mongoose"
+import authRoute from "./routes/auth.js";
+import usersRoute from "./routes/users.js";
+import hotelsRoute from "./routes/hotels.js";
+import roomsRoute from "./routes/rooms.js";
 
 const app = express();
+app.use(express.json());
+
 
 const connect = async () => {
     try {
@@ -16,9 +22,23 @@ mongoose.connection.on('disconnected', () => {
     console.log('MongoDB default connection disconnected');
 });
 
-mongoose.connection.on("connected", () => {
-  console.log(`MongoDB default connection open to MongoDB`);
-});
+
+app.use("/api/auth", authRoute);
+app.use("/api/users", usersRoute);
+app.use("/api/hotels", hotelsRoute);
+app.use("/api/rooms", roomsRoute);
+
+app.use((err, req, res, next) => {
+    const errorStatus = err.status || 500;
+    const errorMessage = err.message || "Something went wrong";
+    res.status(errorStatus).json({ 
+        success: false,
+        status: errorStatus,
+        message: errorMessage,
+        stack: err.stack
+     });
+})
+
 
 app.listen(process.env.PORT, () => {
     connect()
